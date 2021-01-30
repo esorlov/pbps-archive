35067929e4a4ee835cf1d4071e652b204538dae2
Egor S. Orlov <Egor.Orlov@avalon.ru>
2021-01-31 01:00:21 +0300

    Some logging data getted: client ip, resp code, others

--- myweb.c
+++ myweb.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/select.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <microhttpd.h>
@@ -23,7 +24,7 @@
 #define MAP_SIZE 6
 
 struct MHD_Daemon *mht_daemon;
-
+char client_ip[16];
 
 struct cont_element {
   char ext[5];
@@ -51,17 +52,31 @@ char* get_content_type(char *cont_type,
   return cont_type;
 }
 
+int print_out_key (void *cls,
+		   enum MHD_ValueKind kind, 
+                   const char *key,
+		   const char *value) {
+  printf ("%s: %s\n", key, value);
+  return MHD_YES;
+}
 
-int add_to_log (const char *url,
+
+int add_to_log (struct MHD_Connection *connection,
+		const char *url,
 		const char *method,
                 const char *version,
 		const char *upload_data,
-		size_t *upload_data_size) {
+		size_t *upload_data_size,
+		int ret_code) {
 
   (void)upload_data;       /* Unused. Silent compiler warning. */
   (void)upload_data_size;  /* Unused. Silent compiler warning. */
 
-  printf ("New %s request for %s using version %s\n", method, url, version);
+  printf ("%s request from %s for %s using version %s response code %d\n",
+	  method, client_ip, url, version, ret_code);
+  MHD_get_connection_values (connection, MHD_HEADER_KIND,
+			     &print_out_key, NULL);
+
   return MHD_YES;
 }
 
@@ -76,8 +91,7 @@ int construct_filename (char *file_name,
 }
 
 
-static int
-answer_to_connection (void *cls,
+static int answer_to_connection (void *cls,
 		      struct MHD_Connection *connection,
                       const char *url,
 		      const char *method,
@@ -96,9 +110,7 @@ answer_to_connection (void *cls,
   char file_name[256];
   char *content_type;
 
-  add_to_log(url, method, version, upload_data, upload_data_size);
-
-  
+ 
   if (0 != strcmp (method, "GET"))
     return MHD_NO;
 
@@ -126,7 +138,10 @@ answer_to_connection (void *cls,
             MHD_queue_response (connection, MHD_HTTP_INTERNAL_SERVER_ERROR,
                                 response);
           MHD_destroy_response (response);
-
+	  add_to_log(connection,
+		     url, method, version,
+		     upload_data, upload_data_size,
+		     MHD_HTTP_INTERNAL_SERVER_ERROR);
           return ret;
       }
       else
@@ -138,8 +153,19 @@ answer_to_connection (void *cls,
 
   ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
   MHD_destroy_response (response);
-
+  add_to_log(connection,
+	     url, method, version,
+	     upload_data, upload_data_size,
+	     MHD_HTTP_OK);
   return ret;
+}
+
+static int on_client_connect (void *cls,
+                              const struct sockaddr *addr,
+			      socklen_t addrlen) {
+  //printf ("sockaddr: %s\n", inet_ntoa(((struct sockaddr_in *)addr)->sin_addr));
+  strcpy(client_ip, inet_ntoa(((struct sockaddr_in *)addr)->sin_addr));
+  return MHD_YES;
 }
 
 void quit_handler(int s) {
@@ -152,7 +178,7 @@ int main (void) {
   signal(SIGINT, quit_handler);
   signal(SIGTERM, quit_handler);
   mht_daemon = MHD_start_daemon (MHD_USE_INTERNAL_POLLING_THREAD,
-			     PORT, NULL, NULL,
+			     PORT, &on_client_connect, NULL,
                              &answer_to_connection, NULL, MHD_OPTION_END);
   if (NULL == mht_daemon)
     return 1;
